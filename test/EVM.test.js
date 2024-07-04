6b8e62c463ddda8511b35e2340b7f41aad3f2472
mradul_09 <mradulnatani0@gmail.com>
2024-07-04 14:31:03 +0530

    changed dependencies for security updates

--- test/EVM.test.js
+++ test/EVM.test.js
@@ -1,10 +1,12 @@
-const {interfaces,Bytecode} = require('..compile.js');
+const { interfaces, Bytecode } = require('./compile');
 const mocha = require('mocha');
 const ganache = require('ganache');
 const assert = require('assert');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.Provider());
+const web3 = new Web3(ganache.provider());
+
 let EVM;
-beforeEach(async()={
-   EVM : await web3.eth.getAccounts.contracts(JSON.parse(interfaces)).deploy({data:Bytecode,arguments:'hi'})
-})
+
+beforeEach(async () => {
+  // EVM: await web3.eth.getAccounts.contracts(JSON.parse(interfaces)).deploy({ data: Bytecode, arguments: 'hi' })
+});
