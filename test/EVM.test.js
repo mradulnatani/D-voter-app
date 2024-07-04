d50600577b1c847d59021d03ee490839683db7c7
mradul_09 <mradulnatani0@gmail.com>
2024-07-04 20:28:24 +0530

    fixed module not found error

--- test/EVM.test.js
+++ test/EVM.test.js
@@ -1,12 +1,12 @@
-const { interfaces, Bytecode } = require('./compile');
+const { interfaces, Bytecode } = require('C:/Users/mradu/OneDrive/Documents/Solidity and ethereum course/D-voter-app/compile');
 const mocha = require('mocha');
 const ganache = require('ganache');
 const assert = require('assert');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-
 let EVM;
-
+let accounts;
 beforeEach(async () => {
-  // EVM: await web3.eth.getAccounts.contracts(JSON.parse(interfaces)).deploy({ data: Bytecode, arguments: 'hi' })
+  accounts = await web3.eth.getAccounts();
+   EVM = await web3.eth.Contracts(JSON.parse(interfaces)).deploy({ data: Bytecode}).send({from:accounts[0],gas:'1000000'});
 });
